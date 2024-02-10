e1f1aea8be63ec04dd5ab78b4931bf8e95a32e77
Ahmed Bouhlel <a.bouhlel@oodrive.com>
2024-02-11 00:28:53 +0100

    chore: add stylelint configs

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,17 +1,12 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./modules/home/lazy.module').then(m => m.LazyModule) },
   {
-    path: '',
-    component: AppComponent,
-    children: [
-      //      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', loadChildren: () => import('./modules/home/lazy.module').then(m => m.LazyModule) },
-      {
-        path: 'blank-page',
-        loadChildren: () => import('./modules/blank-page/blank-page.module').then((m) => m.BlankPageModule)
-      }
-    ],
+    path: 'blank-page',
+    loadChildren: () => import('./modules/blank-page/blank-page.module').then((m) => m.BlankPageModule)
   }
-]
+];
+
+
